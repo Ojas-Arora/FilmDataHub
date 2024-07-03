33e0cc6072d78f864b0150df021c11476038259a
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-04 00:13:19 +0530

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 # Set page config
 st.set_page_config(page_title="Movies Dataset", page_icon="🎬", layout="wide")
-
+st.markdown("---")
 st.title("🎬 FILMOGRAPHY DATASET")
 st.write(
     """
